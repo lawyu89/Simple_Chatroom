20cc677d2c8377e5c73eb53a1b9637336ee4c626
Lawrence Yu <lawrenceyu1@gmail.com>
2015-05-27 00:45:34 -0700

    Add current user and previous message to db to show users

--- app.js
+++ app.js
@@ -2,17 +2,50 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-
+var redis = require('redis');
+var redisClient = redis.createClient();
+redis.FLUSHDB
 io.on('connection', function(client){
   console.log('Client connected...')
   client.on('join', function(name){
     client.nickname = name
+    redisClient.lrange('messages', 0, -1, function(err, messages){
+      messages = messages.reverse();
+      messages.forEach(function(message){
+        client.emit("messages", JSON.parse(message));
+      })
+    })
+    redisClient.sadd('names', name)
+    redisClient.smembers('names', function(err, names){
+      names.forEach(function(name){
+        client.emit('addUser', name)
+      })
+    })
+    client.broadcast.emit('addUser', name)
     client.emit('messages', {name: name, message: ' has join the chat.'})
     client.broadcast.emit('messages', {name: name, message: ' has join the chat.'})
+  })
+  client.on('addUser', function(name){
+    redisClient.lrange('messages', 0, -1, function(err, messages){
+      messages = messages.reverse();
+      messages.forEach(function(message){
+        client.emit("addUser", message.name);
+      })
+    })
   })
   client.on('messages', function(data){
     client.emit('messages', {name: client.nickname, message: ': '+data})
     client.broadcast.emit('messages', {name: client.nickname, message: ': '+data});
+    var message = JSON.stringify({name: client.nickname, message: ':'+data})
+    redisClient.lpush('messages', message, function(err, res){
+      redisClient.ltrim('messages', 0, 9)
+    })
+  })
+  client.on('disconnect', function(name){
+    redisClient.get(client.nickname, function(err, name){
+      client.broadcast.emit('removeUser', client.nickname)
+      redisClient.srem('names', client.nickname)
+    })
   })
 })
 
